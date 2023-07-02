2832c1487858309fdc0dc1ba1a7b624e3cc1249f
ritik1903 <71216958+ritik1903@users.noreply.github.com>
2023-07-02 15:57:31 +0530

    Performs  important business logic

--- lift/serializer.py
+++ lift/serializer.py
@@ -1,8 +1,8 @@
-# from rest_framework import serializers
-# from .models import Lift
+from rest_framework import serializers
+from .models import Lift
 
 
-# class LiftSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Lift
-#         fields = ("id", "current_floor", "move_up", "door_open", "busy", "is_OOO")
+class LiftSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lift
+        fields = ("id", "current_floor", "move_up", "door_open", "busy", "is_OOO")
